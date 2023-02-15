c26369172bc94aa7f1ca40158290a06fc12ac85d
Facundo David Gonzalez <facundo.gonzalez@devactory.com>
2023-02-15 01:15:43 -0300

    add song

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import { Contact } from './components/Contact/Contact'
+import { MusicPlayer } from './components/MusicPlayer/MusicPlayer'
 import { PartyInfo } from './components/PartyInfo/PartyInfo'
 import { Photos } from './components/Photos/Photos'
 import { Presentation } from './components/Presentation/Presentation'
 
 function App() {
   return (
-    <div className="App">
+    <div className="relative max-w-4xl mx-auto App">
       <Presentation />
       <PartyInfo />
       <Photos />
       <Contact />
+      <MusicPlayer />
     </div>
   )
 }
